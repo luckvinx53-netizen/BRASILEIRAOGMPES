ce617d9e8359d0a555c94e8158e8e37b26fea354
luckvinx53-netizen <luckvinx53@gmail.com>
2026-05-12 23:21:24 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
 const supabase = window.supabaseClient;
-
-const jogos = [
-  {
-    rodada: "17ª Rodada",
-    mandante: "Botafogo",
-    visitante: "Mirassol",
-    placar: "2 x 1"
-  },
-  {
-    rodada: "18ª Rodada",
-    mandante: "Flamengo",
-    visitante: "Palmeiras",
-    placar: "1 x 0"
-  }
-];
-
-console.log("Supabase conectado:", supabase);
-console.log("Jogos:", jogos);
